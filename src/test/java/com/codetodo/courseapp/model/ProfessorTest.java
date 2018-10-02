da5b561b211c32aa777055314e5e0eb29f05d33a
Aldo Ernesto Rodriguez Sixto <aldoernesto.rodriguez@mcbook-aldoernestorodriguez.local>
2018-10-02 16:26:32 +0200

    Add test coverage

--- src/test/java/com/codetodo/courseapp/model/ProfessorTest.java
+++ src/test/java/com/codetodo/courseapp/model/ProfessorTest.java
@@ -12,8 +12,39 @@ public class ProfessorTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInitProfesoor() {
+		String name = "pedro";
+		String email = "pepe@host.com";
+		Long id = 1l;
+		
+		Professor professor = new Professor.Builder().setName(name ).setEmail(email).setId(id ).build();
+		
+		assertEquals(name, professor.getName());
+		assertEquals(email, professor.getEmail());
+		assertEquals(id, professor.getId());
+	}
+	
+	@Test
+	public void testProfessorEquals() {
+		String name = "juan";
+		String email = "juan@host.com";
+		Long id = 2l;
+		
+		Professor professor1 = new Professor.Builder().setName(name ).setEmail(email).setId(id ).build();
+		Professor professor2 = new Professor.Builder().setName(name ).setEmail(email).setId(id ).build();
+		
+		assertTrue(professor1.equals(professor1));
+		assertTrue(professor1.equals(professor2));
+	}
+	
+	@Test
+	public void testHashCode() {
+		String email = "name@host.com";
+		
+		Professor professor1 = new Professor.Builder().setEmail(email).build();
+		Professor professor2 = new Professor.Builder().setEmail(email).build();
+		
+		assertEquals(professor1.hashCode(), professor2.hashCode());
 	}
 
 }
