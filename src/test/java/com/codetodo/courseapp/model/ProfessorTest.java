925e7c61fa0af6efa6f96dd46e79ceffbffa6f71
Aldo Ernesto Rodriguez Sixto <aldoernesto.rodriguez@mcbook-aldoernestorodriguez.local>
2018-10-02 15:25:34 +0200

    Add test coverage

---
+++ src/test/java/com/codetodo/courseapp/model/ProfessorTest.java
@@ -0,0 +1,19 @@
+package com.codetodo.courseapp.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProfessorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
